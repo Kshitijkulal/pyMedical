f2b7b475ff30b3a1736d2c61aa78f68e6ffe1264
nerdparth <parthkapoor488@gmail.com>
2024-08-20 15:01:57 +0530

    made the patients-list and patient adding form

--- patients/models.py
+++ patients/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Patient(models.Model):
+    choices = [('M', "male"),
+               ('F', "female")]
+    name=models.CharField(max_length=100)
+    gender=models.CharField(max_length=1, choices=choices)
+    disease=models.CharField(max_length=200)
+
+    def __str__(self):
+      return self.name
--- patients/views.py
+++ patients/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from .models import Patient
 
 # Create your views here.
 
 def home(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        gender= request.POST.get('gender')
+        disease = request.POST.get('disease')
+        Patient.objects.create(name=name,gender=gender,disease=disease)
+
     return render(request, 'patients.html')
+
+def patients_list(request):
+    Patients = Patient.objects.all()
+    return render(request, "pateints-list.html", {'Patients': Patients})
